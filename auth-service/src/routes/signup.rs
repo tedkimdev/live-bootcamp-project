18f376d75570b8c17e9c9eb197f9b8bcacb2a2b6
ted <dev.ted.kim@gmail.com>
2025-08-18 00:42:49 -0400

    [Sprint 1][Task 6] feat(api): implement sign-up route interface

--- auth-service/src/routes/signup.rs
+++ auth-service/src/routes/signup.rs
@@ -1,5 +1,14 @@
-use axum::{http, response::IntoResponse};
+use axum::{http, response::IntoResponse, Json};
+use serde::Deserialize;
 
-pub async fn signup() -> impl IntoResponse {
+pub async fn signup(Json(_request): Json<SignupRequest>) -> impl IntoResponse {
     http::StatusCode::OK.into_response()
+}
+
+#[derive(Deserialize)]
+pub struct SignupRequest {
+    pub email: String,
+    pub password: String,
+    #[serde(rename = "requires2FA")]
+    pub requires_2fa: bool,
 }
