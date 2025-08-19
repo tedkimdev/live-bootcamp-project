c6bcc4931323f89118eedd106b27ddf9e0c3a752
ted <dev.ted.kim@gmail.com>
2025-08-19 01:07:57 -0400

    fix: fix tests to pass

--- auth-service/tests/api/helpers.rs
+++ auth-service/tests/api/helpers.rs
@@ -29,7 +29,7 @@ impl TestApp {
 
     pub async fn get_root(&self) -> reqwest::Response {
         self.http_client
-            .get(format!("{}/", &self.address))
+            .get(format!("{}/auth", &self.address))
             .send()
             .await
             .expect("Failed to execute request.")
